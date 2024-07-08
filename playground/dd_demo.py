73d659cbbb663c4a3ec2ee6856420e0a31f88a0a
Nelsonvon <nelsonvonsd@hotmail.com>
2024-07-08 14:57:41 +0200

    init playground

---
+++ playground/dd_demo.py
@@ -0,0 +1,17 @@
+from dd.autoref import BDD
+
+bdd = BDD()
+bdd.declare('col_y','col_w','tag_o','tag_b')
+kb = bdd.add_expr(r"(col_y&tag_o)|(col_w&tag_b)")
+
+rule1 = bdd.add_expr(r"col_y=>(tag_o&!tag_b)")
+rule2 = bdd.add_expr(r"col_w=>(tag_b&!tag_o)")
+kb2 = bdd.apply('and',rule1,rule2)
+
+anorm = bdd.add_expr(r"col_y&tag_b")
+print(bdd.apply('and',kb2,anorm).to_expr())
+
+# MDD
+import mdd
+
+
