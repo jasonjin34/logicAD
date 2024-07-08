28175efd59a8e4b9edba2346424ffa570c87b036
Jin Er <er.jin@lfb.rwth-aachen.de>
2024-07-08 21:15:07 +0200

    add juice bottle prompt

--- src/anomalib/models/logicad/text_prompt.py
+++ src/anomalib/models/logicad/text_prompt.py
@@ -5,7 +5,7 @@
 
 TEXT_EXTRACTOR_PROMPTS = {
     "breakfast_box": "what is on the left side of image? and what is on the right side of image?",
-    "juice_bottle": None,
+    "juice_bottle": "Describe this juice bottle's characteristics, is the bottle full? locations of label and is objects",
     "pushpins": None,
     "screw_bag": None,
     "splicing_connectors": None,
@@ -13,7 +13,7 @@ TEXT_EXTRACTOR_PROMPTS = {
 
 TEXT_SUMMATION_PROMPTS = {
     "breakfast_box": "number of objects, and their location",
-    "juice_bottle": None,
+    "juice_bottle": "location of objects, and their characteristics",
     "pushpins": None,
     "screw_bag": None,
     "splicing_connectors": None,
