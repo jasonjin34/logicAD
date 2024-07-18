bcafff987911522f2403d70299be2b0e0d9c180e
Jin Er <er.jin@lfb.rwth-aachen.de>
2024-07-18 20:40:33 +0200

    mvtec and visa dataset and clean the repo

--- src/anomalib/models/logicad/text_prompt.py
+++ src/anomalib/models/logicad/text_prompt.py
@@ -4,11 +4,18 @@
 # as for now the text prompt is class dependent we can eventually make it more general
 
 TEXT_EXTRACTOR_PROMPTS = {
+    # loco category
     "breakfast_box": "what is on the left side of image? and what is on the right side of image?",
     "juice_bottle": "Describe this juice bottle's characteristics, is the bottle full? locations of label and is objects",
     "pushpins": "are different pushpins divided by plastic wall?, or only contain one pushpin?",
     "screw_bag": "What are inside the bag?",
-    "splicing_connectors": "Describe the splicing connectors and cables in the image. Is the connector symmetric? Is the cable symmetric? Is the cable broken? connector should be far away from each other"
+    "splicing_connectors": "Describe the splicing connectors and cables in the image. Is the connector symmetric? Is the cable symmetric? Is the cable broken? connector should be far away from each other",
+    # mvtec category
+    # "cable":  "is the cable image flawless? if the following conditionals are reach, then the image is flawness or defect?, (bent wire, cable with less than 3 color pattern, cut_inner_insulation, missing cable, scratch), give me only short answer and reason",
+    "cable":  "is the cable intact or flawless? Are the 3 inner cables intact and distinguishable? is there any crack or broken part?",
+    "pill": "is the pill flawless? if all the following conditions need are reach, no scratch, contanimination, no yellow spot, no crack, no fault print",
+    "capsule": "does the capsule image contain scratch? does all parts intact or flawless? does the 500 print intact?",
+    "screw": "does the screw contains the following anomalies, broken thread side, flaw thread top, scratch and broken front"
 }
 
 TEXT_SUMMATION_PROMPTS = {
@@ -17,6 +24,11 @@ TEXT_SUMMATION_PROMPTS = {
     "pushpins": "number of pushpins, and patches characteristics",
     "screw_bag": "number of objects",
     "splicing_connectors": "connector: {symmetric or not, number of connectors}, cable: {nummber of cables, symmetric:, broken or not?, length: longer than connector}",
+    # mvtec category
+    "cable":  "is the object intact, flawless or perfect?",
+    "pill":  "conditions: true or false",
+    "capsule":  "is the object intact, flawless or perfect?",
+    "screw":  "flaw: {extend of flaw}",
 }
 
 TEXT_NUM_OBJECTS_PROMPTS = {
